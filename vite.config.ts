66ba7005f9a328bef5430d92a5a481b1f18e986e
Anirban Sarkar <137922245+Anirban780@users.noreply.github.com>
2025-05-16 21:53:59 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/DSA-Sheet-Todo-List/'
   plugins: [
     react(),
     tailwindcss(),
